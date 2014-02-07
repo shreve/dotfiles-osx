47d29621b4bf5fa29b671a0f7497cefbb5fad51e
Jacob Evan Shreve <jacob@shreve.ly>
2014-02-07 17:12:33 -0500

    Add dotjs for Pivotal Tracker

---
+++ js/pivotaltracker.com.js
@@ -0,0 +1,39 @@
+/* dotjs for Pivotal Tracker
+ *
+ * I like to be able to cmd+enter to submit forms, so I added a simple
+ * implementation of this. Pivotal fires a document.ready before the
+ * project interface is loaded, so I need to search for editors until
+ * they're loaded.
+ */
+
+console.log('~/.js/pivotaltracker.com.js loaded');
+
+var $editors = [],
+    editorSearch,
+    bindEditors,
+    log;
+
+log = function() {
+  console.log('pivotaltracker.com.js', arguments);
+}
+
+editorSearch = setInterval(function() {
+  if($editors.length == 0) {
+    $editors = $('textarea.editor')
+    log("checking");
+  } else {
+    bindEditors();
+    clearInterval(editorSearch);
+    log("found editors");
+  }
+}, 1000);
+
+bindEditors = function() {
+  $editors.bind('keypress', function(e) {
+    if(e.metaKey && (e.keyCode || e.which) == 13) {
+      id = this.id.replace('text', 'button')
+      add = $('#'+id);
+      add.click();
+    }
+  });
+};
